b979214fd7e4af62bb08bb4e130ed61cf58b40ef
Fathony Teguh Irawan <fathony.teguh@gmail.com>
2024-03-10 19:07:22 +0900

    Migrate to Anvil 2.4.9

--- anvil-helper/processor/build.gradle.kts
+++ anvil-helper/processor/build.gradle.kts
@@ -22,18 +22,18 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
 }
 
 dependencies {
-    implementation("com.squareup.anvil:compiler-api:2.4.7")
-    implementation("com.squareup.anvil:compiler-utils:2.4.7")
+    implementation("com.squareup.anvil:compiler-api:2.4.9")
+    implementation("com.squareup.anvil:compiler-utils:2.4.9")
     implementation(project(":api"))
 
     implementation("com.google.dagger:dagger:2.48")
     compileOnly("com.google.auto.service:auto-service-annotations:1.0.1")
     kapt("com.google.auto.service:auto-service:1.0.1")
 
-    implementation("com.squareup:kotlinpoet:1.13.0")
-    implementation("com.squareup:kotlinpoet-metadata:1.13.0")
+    implementation("com.squareup:kotlinpoet:1.15.3")
+    implementation("com.squareup:kotlinpoet-metadata:1.15.3")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("com.google.truth:truth:1.1.3")
-    testImplementation(testFixtures("com.squareup.anvil:compiler-utils:2.4.4"))
+    testImplementation(testFixtures("com.squareup.anvil:compiler-utils:2.4.9"))
 }
--- anvil-helper/processor/src/test/kotlin/dev/fathony/anvil/helper/processor/TestUtils.kt
+++ anvil-helper/processor/src/test/kotlin/dev/fathony/anvil/helper/processor/TestUtils.kt
@@ -2,26 +2,27 @@ package dev.fathony.anvil.helper.processor
 
 import com.squareup.anvil.annotations.ExperimentalAnvilApi
 import com.squareup.anvil.compiler.api.CodeGenerator
+import com.squareup.anvil.compiler.internal.testing.AnvilCompilationMode
 import com.squareup.anvil.compiler.internal.testing.compileAnvil
-import com.tschuchort.compiletesting.KotlinCompilation
+import com.tschuchort.compiletesting.CompilationResult
+import com.tschuchort.compiletesting.JvmCompilationResult
 import org.intellij.lang.annotations.Language
 import org.jetbrains.kotlin.compiler.plugin.ExperimentalCompilerApi
 
 @OptIn(ExperimentalCompilerApi::class, ExperimentalAnvilApi::class)
 internal fun compile(
     @Language("kotlin") vararg sources: String,
-    previousCompilationResult: KotlinCompilation.Result? = null,
+    previousCompilationResult: JvmCompilationResult? = null,
     enableDebuggerAnnotationProcessor: Boolean = false,
     codeGenerators: List<CodeGenerator> = emptyList(),
     allWarningAsErrors: Boolean = false,
-    moduleName: String? = null,
-    block: KotlinCompilation.Result.() -> Unit = {}
-): KotlinCompilation.Result = compileAnvil(
+    mode: AnvilCompilationMode = AnvilCompilationMode.Embedded(codeGenerators),
+    block: JvmCompilationResult.() -> Unit = {}
+): CompilationResult = compileAnvil(
     sources = sources,
     allWarningsAsErrors = allWarningAsErrors,
     previousCompilationResult = previousCompilationResult,
     enableDaggerAnnotationProcessor = enableDebuggerAnnotationProcessor,
-    codeGenerators = codeGenerators,
-    moduleName = moduleName,
+    mode = mode,
     block = block
 )
